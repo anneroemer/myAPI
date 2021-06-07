bb149bf5e8f06329966efcb0a282f73d53fa90e0
anneroemer <77618343+anneroemer@users.noreply.github.com>
2021-06-07 09:10:34 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express"); //express er serveren
 const formidable = require("express-formidable");
+const cors = require("cors");
 const animals = require("./routes/animals"); //node ved at det er en js fil
 const foods = require("./routes/foods");
 const accessories = require("./routes/accessories");
@@ -17,6 +18,10 @@ const app = express();
 
 //import db-connection
 require("./database");
+
+app.use("/", express.static('docs'));
+
+app.use(cors());
 
 //parse http form data
 app.use(formidable());
